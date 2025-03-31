59df898d76e8539a91560a0c95fc34fd409f9039
ishagautamMSFT <93327866+ishagautamMSFT@users.noreply.github.com>
2025-03-31 22:42:04 +0530

    Added templates (#13)

--- ACPP.Client/src/config.ts
+++ ACPP.Client/src/config.ts
@@ -1,3 +1,5 @@
+import { EMPTY_STRING } from "./common/helpers/Constants";
+
 const appConfig = {
   privacyPolicyLink: 'https://privacy.microsoft.com/en-US/data-privacy-notice',
   termsOfUseLink: 'https://www.microsoft.com/en-us/servicesagreement',
@@ -13,13 +15,13 @@ const appConfig = {
 };
 
 const config = {
-  teamsAppId: process.env.REACT_APP_TEAMS_APP_ID,
+  teamsAppId: process.env.REACT_APP_TEAMS_APP_ID !== '$(teamsAppId)' ? process.env.REACT_APP_TEAMS_APP_ID : EMPTY_STRING,
   clientId: process.env.REACT_APP_CLIENT_ID,
   aiConnectionString: process.env.REACT_APP_AI_CONNECTION_STRING,
   defaultTokenScope: process.env.REACT_APP_DEFAULT_TOKEN_SCOPE,
-  adminDefaultEnv: process.env.REACT_APP_ADMIN_ENV,
-  feedbackFormUrl: process.env.REACT_APP_FEEDBACK_FORM_URL,
-  faqUrl: process.env.REACT_APP_FAQ_URL,
+  adminDefaultEnv: process.env.REACT_APP_ADMIN_ENV !== '$(adminEnvironment)' ? process.env.REACT_APP_ADMIN_ENV : EMPTY_STRING,
+  feedbackFormUrl: process.env.REACT_APP_FEEDBACK_FORM_URL !== '$(feedBackFormUrl)' ? process.env.REACT_APP_FEEDBACK_FORM_URL : EMPTY_STRING,
+  faqUrl: process.env.REACT_APP_FAQ_URL != '$(faqUrl)' ? process.env.REACT_APP_FAQ_URL : EMPTY_STRING,
   appTitle: 'Automation Catalog for Power Platform',
   authStartPage: 'auth-start.html',
   apiEndpoint: window.location.origin,
